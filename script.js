2518511191a60274b737d981c4e195d047e5bfeb
mjdownes <molly@tritonehealth.com>
2026-02-06 16:20:15 +0000

    Updated to add animation to yes button on hover and the no button moves when the mouse enters

---
+++ script.js
@@ -0,0 +1,33 @@
+const noButton = document.querySelector('.runaway');
+noButton.addEventListener('click', noClicked);
+const container = document.querySelector('.question');
+const yesButton = document.querySelector('.button-yes');
+yesButton.addEventListener('click', yesClicked);
+
+
+function yesClicked() {
+    document.querySelector('.yes').style.display = 'block';
+    document.querySelector('.question-container').style.display = 'none';
+}
+
+function noClicked() {
+    alert('This is not an option, please reconsider! ❤️');
+    noButton.style.transform = `translate(0px, 0px)`;
+}
+
+
+noButton.addEventListener('mouseover', () => {
+    const buttonRect = noButton.getBoundingClientRect();
+
+    const padding = 20
+    const maxX = window.innerWidth - buttonRect.width - padding;
+    const maxY = window.innerHeight - buttonRect.height - padding;
+
+    const x = Math.random() * maxX + padding;
+    const y = Math.random() * maxY + padding;
+
+    noButton.style.position = `fixed`;
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
+
+});
